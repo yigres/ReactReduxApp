e1c096130c19c0ee83d80e25edca06007b7d7940
Sergey Kirillov <s_kirillov@rambler.ru>
2021-07-19 14:13:36 +0300

    added pagination

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
+import { setCurrentPage } from "../../reducers/reposReducer";
 import { getRepos } from "../actions/repos";
 import "./main.less"
 import Repo from "./repo/Repo";
+import { createPages } from "../../utils/pagesCreator";
 
 const Main = () => {
   const dispatch = useDispatch();
   const repos = useSelector((state) => state.repos.items);
   const isFetching = useSelector((state) => state.repos.isFetching);
+  const currentPage = useSelector((state) => state.repos.currentPage);
+  const totalCount = useSelector((state) => state.repos.totalCount);
+  const perPage = useSelector((state) => state.repos.perPage);
   const [searchValue, setSearchValue] = useState("");
+  const pagesCount = Math.ceil(totalCount / perPage);
+  const pages = [];
+  createPages(pages, pagesCount, currentPage);
 
   useEffect(() => {
-    dispatch(getRepos());
-  }, []);
+    dispatch(getRepos(searchValue, currentPage, perPage));
+  }, [currentPage]);
 
   function searchHanler() {
-    dispatch(getRepos(searchValue));
+    dispatch(setCurrentPage(1));
+    dispatch(getRepos(searchValue, currentPage, perPage));
   }
 
   return (
@@ -34,6 +43,13 @@ const Main = () => {
 
         </div>
       }
+
+      <div className="pages">
+        { pages.map((page, index) => <span
+            key={index}
+            className={currentPage === page ? "current-page" : "page"}
+            onClick={() => dispatch(setCurrentPage(page))}>{page}</span>)}
+      </div>
     </div>
   )
 }
